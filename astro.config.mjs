85689a4e4d74ec9df0c8bb4e857d9fc899129201
rafaelm71 <39887744+rafaelm71@users.noreply.github.com>
2025-04-14 22:42:08 -0400

    implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rafaelm71.github.io',
+  base: 'my-repo',
   vite: {
     plugins: [tailwindcss()]
   }
+
+  
 });
