a21355dafaac5336f1b18617abf45a4922509f4b
rafaelm71 <39887744+rafaelm71@users.noreply.github.com>
2025-04-23 20:33:51 -0400

    change

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rafaelm71.github.io',
   base: 'GM-Geeks-page',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
 
-  
+  integrations: [react()]
 });
